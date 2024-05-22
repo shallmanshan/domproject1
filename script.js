28b954a739104d431ccfa5ac5a94934c5b13ec72
shallmanshan <shallmanshan@gmail.com>
2024-05-22 22:12:12 +0530

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var one = document.getElementById("one");
+one.style.backgroundColor = "yellow";
+one.style.margin = "20px";
+one.style.padding = "10px";
+one.style.fontSize = "18px";
+one.style.fontWeight = "bold";
+one.style.height = "200px";
+one.style.width = "300px";
+
